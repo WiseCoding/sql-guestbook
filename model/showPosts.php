001e53e32200276b7665a6127f8d917036ccbb07
Mattias Bonte <wisecodr@gmail.com>
2020-10-07 09:57:08 +0200

    Test DB

--- model/showPosts.php
+++ model/showPosts.php
@@ -25,7 +25,7 @@ class showPosts
 
   public function openConnection(): PDO
   {
-    $db = parse_url(getenv("postgresql-rugged-74301"));
+    $db = parse_url(getenv("postgres://eiyngzmixlorqx:462af116bd57e87e53e6671a05d8955c5b65b79db70e9247d3ce03a7fcbfb2ea@ec2-54-155-22-153.eu-west-1.compute.amazonaws.com:5432/de7j952d2j8cmk"));
 
     $pdo = new PDO("pgsql:" . sprintf(
       "host=%s;port=%s;user=%s;password=%s;dbname=%s",
